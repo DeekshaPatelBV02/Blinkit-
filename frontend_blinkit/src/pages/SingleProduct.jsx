8a52cb55946ff9b7942a0c17a87658e9d7a69603
Deekshapatel2004 <deekshapatelbv2019@gmail.com>
2026-04-13 17:14:27 +0530

    update frontend api urls

--- frontend_blinkit/src/pages/SingleProduct.jsx
+++ frontend_blinkit/src/pages/SingleProduct.jsx
@@ -1,15 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import"../styles/singleproduct.css";
+import "../styles/singleproduct.css";
 import Header from "../components/Header";
 import { CartContext } from "../Features/ContextProvider";
 
-
 function SingleProduct() {
-  const{dispatch}=useContext(CartContext)
+  const { dispatch } = useContext(CartContext);
   const { id } = useParams();
-  const [product, setProduct] = useState(null); 
+  const [product, setProduct] = useState(null);
 
   useEffect(() => {
     getProduct();
@@ -17,40 +16,51 @@ function SingleProduct() {
 
   const getProduct = async () => {
     try {
-      const res = await axios.get(`https://blinkit-3-qi0k.onrender.comhttps://blinkit-3-qi0k.onrender.com/products/${id}`);
-      console.log(res.data); 
+      const res = await axios.get(
+        `https://blinkit-3-qi0k.onrender.com/products/${id}`
+      );
+      console.log(res.data);
       setProduct(res.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  if (!product) 
-    return <h2>Loading...</h2>; 
+  if (!product) return <h2>Loading...</h2>;
 
   return (
     <>
-    <Header/>
-    <div className="single-product-page">
-      <div className="single-image">
-        <img
-          src={`https://blinkit-3-qi0k.onrender.com/images/${product.file}`}
-          alt={product.name}
-        />
-      </div>
+      <Header />
+      <div className="single-product-page">
+        <div className="single-image">
+          <img
+            src={`https://blinkit-3-qi0k.onrender.com/images/${product.file}`}
+            alt={product.name}
+          />
+        </div>
 
-      <div className="information">
-        <h2>{product.name}</h2>
-        <p className="price">₹{product.price}</p>
-        <p>{product.description}</p>
-        <button className="buy-btn"onClick={() =>dispatch({type: "Add",product: {
-        _id: product._id,
-        name: product.name,
-        price: product.price,
-        file: product.file
-        }})}>Add to Cart</button>
+        <div className="information">
+          <h2>{product.name}</h2>
+          <p className="price">₹{product.price}</p>
+          <p>{product.description}</p>
+          <button
+            className="buy-btn"
+            onClick={() =>
+              dispatch({
+                type: "Add",
+                product: {
+                  _id: product._id,
+                  name: product.name,
+                  price: product.price,
+                  file: product.file,
+                },
+              })
+            }
+          >
+            Add to Cart
+          </button>
+        </div>
       </div>
-    </div>
     </>
   );
 }
