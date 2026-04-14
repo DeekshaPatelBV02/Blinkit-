a72073e07574a566b14783e70e97ba9b64e15579
Deekshapatel2004 <deekshapatelbv2019@gmail.com>
2026-04-14 19:02:26 +0530

    fix gmail

--- backend_blinkit/controllers/sendEmail.js
+++ backend_blinkit/controllers/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  service: "gmail",
   port: 587,
   secure: false,
   auth: {
