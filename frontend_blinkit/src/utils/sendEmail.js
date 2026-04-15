1302dafefdb5dcc679c7dc1bae76edae17808a14
Deekshapatel2004 <deekshapatelbv2019@gmail.com>
2026-04-15 16:43:55 +0530

    changes in sendEmail.js

---
+++ frontend_blinkit/src/utils/sendEmail.js
@@ -0,0 +1,23 @@
+import emailjs from "@emailjs/browser";
+
+export const sendEmail = async ({ name, email, amount, payment }) => {
+  try {
+    const response = await emailjs.send(
+      "service_kfa6rf7",
+      "template_k2hj38r",
+      {
+        name,
+        email,
+        amount,
+        payment,
+      },
+      "eDG7KnyyjhD8muxij"
+    );
+
+    console.log("Email sent successfully", response);
+    return response;
+  } catch (error) {
+    console.error("Email failed", error);
+    throw error;
+  }
+};
