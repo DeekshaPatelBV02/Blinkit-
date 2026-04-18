691b343cb424273e9071e3ef0b37dc2369d6affb
Deekshapatel2004 <deekshapatelbv2019@gmail.com>
2026-04-18 19:37:48 +0530

    deploy

--- backend_blinkit/controllers/sendEmail.js
+++ backend_blinkit/controllers/sendEmail.js
@@ -1,13 +1,12 @@
-const nodemailer = require('nodemailer');
-
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    user: (process.env.EMAIL_USER),
-    pass: (process.env.EMAIL_PASS)
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
@@ -23,9 +22,10 @@ async function sendMail(to, subject, html) {
     });
 
     console.log("Email sent:", info.response);
-
   } catch (error) {
     console.log("Email sending failed:", error.message);
+    throw error;
   }
 }
-module.exports = sendEmail;
+
+module.exports = sendMail;
